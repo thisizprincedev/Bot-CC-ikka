b36f85366bd873da9f4d94080e86c4b12c76b963
Prince <princedev432@gmail.com>
2023-03-31 16:14:06 +0530

    Bin Setup Changed

--- mills/plugins/checkers/utils/bininfo.py
+++ mills/plugins/checkers/utils/bininfo.py
@@ -1,4 +1,4 @@
-
+import requests
 from mills.plugins import BINS_DICT
 
 
@@ -7,7 +7,47 @@ def get_bin_info(bin_to_find: int):
         xx = BINS_DICT[str(bin_to_find[:6])]
         return xx if not xx['prepaid'] else False
     else:
-        return False
+        url="https://lookup.binlist.net/"+bin_to_find
+        r = requests.get(url).json()
+
+        try:
+            scheme = r["scheme"]
+        except:
+            scheme = 'None'
+        try:
+            type = r['type']
+        except:
+            type='None'
+        try:
+            brand = r['brand']
+        except:
+            brand ='brand'    
+        try:
+            country = r["country"]["name"]
+            emoji = r["country"]['emoji']
+            alpha2 = r["country"]['alpha2']
+        except:
+            country='None'
+            emoji='None'
+            alpha2="None"
+        try:
+            bank = r['bank']['name']
+        except:
+            bank = 'None'
+        try:
+            url = r['bank']['url']
+        except:
+            url = 'None'
+        return {
+            "country": country,
+            "iso": alpha2,
+            "flag": emoji,
+            "vendor": scheme,
+            "type": type,
+            "level": brand,
+            "bank_name": bank,
+            "prepaid": True if type == "PREPAID" else False
+        }
 
 def get_bin_info_all(bin_to_find):
     
